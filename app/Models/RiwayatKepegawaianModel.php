c9218df3adc143f7e7ee483fabe04377138dad95
Shafirasal <shafirasalsabila15@gmail.com>
2025-07-09 13:59:22 +0700

    model dan index riwayat pegawai

--- app/Models/RiwayatKepegawaianModel.php
+++ app/Models/RiwayatKepegawaianModel.php
@@ -8,22 +8,44 @@ use Illuminate\Database\Eloquent\Model;
 class RiwayatKepegawaianModel extends Model
 {
     use HasFactory;
-    // Relasi ke pegawai (many → one)
-public function pegawai()
-{
-    return $this->belongsTo(PegawaiModel::class, 'nip', 'nip');
-}
 
-// Relasi ke golongan (one → one, FK di sini)
-public function golongan()
-{
-    return $this->belongsTo(GolonganModel::class, 'id_golongan', 'id_golongan');
-}
+    protected $table = 't_riwayat_kepegawaian';
+    protected $primaryKey = 'id_riwayat_kepegawaian';
+    public $timestamps = true;
 
-// Relasi ke jenis kenaikan pangkat (one → one, FK di sini)
-public function jenisKenaikanPangkat()
-{
-    return $this->belongsTo(JenisKenaikanPangkatModel::class, 'id_jenis_kp', 'id_jenis_kp');
-}
+    protected $fillable = [
+        'nip',
+        'file',
+        'id_golongan',
+        'id_jenis_kp',
+        'masa_kerja_tahun',
+        'masa_kerja_bulan',
+        'tmt_pangkat',
+        'keterangan',
+        'aktif'
+    ];
 
+    /**
+     * Relasi ke tabel pegawai (many to one)
+     */
+    public function pegawai()
+    {
+        return $this->belongsTo(PegawaiModel::class, 'nip', 'nip');
+    }
+
+    /**
+     * Relasi ke tabel golongan (one to one)
+     */
+    public function golongan()
+    {
+        return $this->belongsTo(GolonganModel::class, 'id_golongan', 'id_golongan');
+    }
+
+    /**
+     * Relasi ke tabel jenis kenaikan pangkat (one to one)
+     */
+    public function jenisKenaikanPangkat()
+    {
+        return $this->belongsTo(JenisKenaikanPangkatModel::class, 'id_jenis_kp', 'id_jenis_kp');
+    }
 }
---
+++ resources/views/riwayat_kepegawaian/index.blade.php
@@ -0,0 +1,160 @@
+@extends('layouts.template')
+
+@section('title')
+| Riwayat Kepegawaian
+@endsection
+
+@push('css')
+{{-- 
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.bootstrap4.min.css"> 
+--}}
+@endpush
+
+@section('content')
+<section class="section">
+  <div class="section-header">
+    <h1>{{ $breadcrumb->title ?? 'Riwayat Kepegawaian' }}</h1>
+    @include('layouts.breadcrumb', ['list' => $breadcrumb->list])
+  </div>
+
+  <div class="section-body">
+    <div class="col-12">
+      <div class="card">
+        <div class="card-header">
+          <h4>Data Riwayat Kepegawaian</h4>
+          <div class="card-header-action ml-auto">
+            <button onclick="modalAction(`{{ url('/riwayat_kepegawaian/create') }}`)" class="btn btn-primary">
+              <i class="fas fa-plus"></i> Tambah
+            </button>
+          </div>
+        </div>
+
+        <div class="card-body">
+          <div class="table-responsive">
+            <table class="table table-bordered table-hover table-striped dt-responsive nowrap" id="table_riwayat" style="width:100%">
+              <thead class="thead-light">
+                <tr>
+                  <th>#</th>
+                  <th>Nama Pegawai</th>
+                  <th>Golongan</th>
+                  <th>Jenis Kenaikan Pangkat</th>
+                  <th>TMT Pangkat</th>
+                  <th>Masa Kerja</th>
+                  <th>File</th>
+                  <th>Keterangan</th>
+                  <th>Aktif</th>
+                  <th>Aksi</th>
+                </tr>
+              </thead>
+              <tbody>
+                {{-- Data dimuat dinamis via DataTables --}}
+              </tbody>
+            </table>
+          </div>
+        </div>
+
+        <div class="card-footer text-right">
+          {{-- Pagination otomatis oleh DataTables --}}
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<div id="myModal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true"></div>
+@endsection
+
+@push('js')
+<script>
+  $.ajaxSetup({
+    headers: {
+      'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+  });
+
+  function modalAction(url = '') {
+    $('#myModal').load(url, function () {
+      $('#myModal').modal('show');
+    });
+  }
+
+  var dataRiwayatKepegawaian;
+
+  $(document).ready(function () {
+    dataRiwayatKepegawaian = $('#table_riwayat').DataTable({
+      processing: true,
+      serverSide: true,
+      responsive: true,
+      ajax: {
+        url: "{{ url('/riwayat_kepegawaian/list') }}",
+        type: "POST"
+      },
+      columns: [
+        {
+          data: 'DT_RowIndex',
+          className: 'text-center',
+          orderable: false,
+          searchable: false
+        },
+        {
+          data: 'nama_pegawai',
+          className: '',
+          orderable: true,
+          searchable: true
+        },
+        {
+          data: 'nama_golongan',
+          className: '',
+          orderable: true,
+          searchable: true
+        },
+        {
+          data: 'nama_jenis_kp',
+          className: '',
+          orderable: true,
+          searchable: true
+        },
+        {
+          data: 'tmt_pangkat',
+          className: 'text-center',
+          orderable: true,
+          searchable: true
+        },
+        {
+          data: null,
+          render: function (data, type, row) {
+            return row.masa_kerja_tahun + ' th ' + row.masa_kerja_bulan + ' bln';
+          },
+          className: 'text-center'
+        },
+        {
+          data: 'file',
+          className: '',
+          render: function (data, type, row) {
+            return data ? `<a href="/storage/${data}" target="_blank">Lihat File</a>` : '-';
+          }
+        },
+        {
+          data: 'keterangan',
+          className: '',
+          orderable: true,
+          searchable: true
+        },
+        {
+          data: 'aktif',
+          className: 'text-center',
+          orderable: false,
+          searchable: false
+        },
+        {
+          data: 'aksi',
+          className: 'text-center',
+          orderable: false,
+          searchable: false
+        }
+      ]
+    });
+  });
+</script>
+@endpush
