4e655da179faee0514ad0d95078c228004e0dbf0
Shafirasal <shafirasalsabila15@gmail.com>
2025-07-14 10:04:39 +0700

    penamaan file pada view edit riwayat pegawai

--- resources/views/riwayat_kepegawaian/edit.blade.php
+++ resources/views/riwayat_kepegawaian/edit.blade.php
@@ -86,6 +86,25 @@
                     @endif
                 </div>
 
+
+                {{-- <div class="form-group">
+                    <label>File (Kosongkan jika tidak diubah)</label>
+                    <input type="file" name="file" class="form-control">
+
+                    @php
+                        $fileLama = basename($kepegawaian->file);
+                    @endphp
+
+                    @if ($kepegawaian->file)
+                        <small>
+                            <a href="{{ asset('storage/' . $kepegawaian->file) }}" target="_blank">
+                                {{ $fileLama }}
+                            </a>
+                        </small>
+                    @endif
+                </div> --}}
+
+
                 {{-- <div class="form-group">
                     <label for="file">Bukti Dokumen (PDF)</label>
                     <input type="file" name="file" id="file" accept=".pdf" class="form-control">
