bbab6130db299ec2057534ae20eb9b59c67a2033
tambakboii <athiyanaqil004@gmail.com>
2025-08-12 12:28:40 +0700

    seeder -> database seeder: tambah pemanggilan rekening seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -33,6 +33,7 @@ class DatabaseSeeder extends Seeder
             ProgramSeeder::class,
             KegiatanSeeder::class,
             MasterSubKegiatanSeeder::class,
+            RekeningSeeder::class,
         ]);
     }
 }
