e5d57442806f814e28dbe2695b96929392f62701
Shafirasal <shafirasalsabila15@gmail.com>
2025-08-13 10:37:42 +0700

    kegiatan view index -> perbaikan ukuran filter form

--- resources/views/kegiatan/index.blade.php
+++ resources/views/kegiatan/index.blade.php
@@ -24,14 +24,18 @@
                     </div>
 
                     <div class="card-body">
-                        <div class="form-group d-flex align-items-center mb-0">
-                            <label for="program_nama" class="mb-0 mr-2" style="width: 160px;"><strong>Nama Program:</strong></label>
-                            <select id="program_nama" class="form-control form-control-sm" style="width: 100px;">
-                                <option value="">-- Pilih Program --</option>
-                                @foreach ($listProgram as $program)
-                                    <option value="{{ $program->nama_program }}">{{$program->kode_program}} - {{ $program->nama_program }}</option>
-                                @endforeach
-                            </select>
+                        <div class="row mb-3">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="program_nama" class="mb-0 mr-2" style="width: 160px;"><strong>Nama Program:</strong></label>
+                                    <select id="program_nama" class="form-control form-control-sm" style="width: 100px;">
+                                        <option value="">-- Pilih Program --</option>
+                                        @foreach ($listProgram as $program)
+                                            <option value="{{ $program->nama_program }}">{{$program->kode_program}} - {{ $program->nama_program }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
                         </div>
 
 
