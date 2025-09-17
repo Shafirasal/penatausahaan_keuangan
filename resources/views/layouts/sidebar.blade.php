e7797552c1994da5de408d411d85694faa3c5c14
tambakboii <athiyanaqil004@gmail.com>
2025-09-17 15:57:31 +0700

    layout sidebar -> `Update layout files with minor changes`

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -4,7 +4,7 @@
             <a href="index.html">BIRO PBJ</a>
           </div>
           <div class="sidebar-brand sidebar-brand-sm">
-            <a href="index.html">St</a>
+            <a href="index.html">BPBJ</a>
           </div>
 
         {{--sidebar menu--}}
