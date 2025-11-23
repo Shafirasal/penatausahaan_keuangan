946fbc6f7d39bc5614184a9ac62fcf3c78d5b73d
Shafirasal <shafirasalsabila15@gmail.com>
2025-11-23 19:57:51 +0700

    footer -> ubah copyright

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,7 @@
 <footer class="main-footer">      
       <div class="footer-left">
-          Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauv.al/">Tim IT</a>
+          {{-- Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauv.al/">Tim IT</a> --}}
+               Copyright &copy; 2025 <div class="bullet"></div> Developed by shafira wisam athiyan</a>
         </div>
         <div class="footer-right">
           2.3.1
